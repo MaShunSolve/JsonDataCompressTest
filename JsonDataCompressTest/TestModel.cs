e36d68a25312c2ec182681b239e004b769f80937
eddy <eddyhsieh@mail.kway.com.tw>
2022-12-20 19:37:54 +0800

    加入專案檔案。

---
+++ JsonDataCompressTest/TestModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDataCompressTest
+{
+    /// <summary>
+    /// 測試訊息Model
+    /// </summary>
+    public class TestModel
+    {
+        public string TestMessage01 { get; set; }
+        public string TestMessage02 { get; set; }
+        public string TestMessage03 { get; set; }
+        public string TestMessage04 { get; set; }
+        public string TestMessage05 { get; set; }
+        public string TestMessage06 { get; set; }
+        public string TestMessage07 { get; set; }
+        public string TestMessage08 { get; set; }
+        public string TestMessage09 { get; set; }
+        public string TestMessage10 { get; set; }
+        public string TestMessage11 { get; set; }
+        public string TestMessage12 { get; set; }
+        public string TestMessage13 { get; set; }
+        public string TestMessage14 { get; set; }
+        public string TestMessage15 { get; set; }
+        public string TestMessage16 { get; set; }
+        public string TestMessage17 { get; set; }
+        public string TestMessage18 { get; set; }
+        public string TestMessage19 { get; set; }
+        public string TestMessage20 { get; set; }
+    }
+}
